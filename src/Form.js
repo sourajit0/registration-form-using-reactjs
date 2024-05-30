577d51a5dab1e366c8fd2b539caa0b4b47cbf6ad
sourajit0 <164853152+sourajit0@users.noreply.github.com>
2024-05-30 19:55:43 +0530

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Form.css"; // Import your CSS file
+import "./Form.css"; 
 
 const Form = () => {
   const navigate = useNavigate();
